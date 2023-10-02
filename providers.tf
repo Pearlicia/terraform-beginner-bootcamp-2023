2324036895dafc7a7ea8c5a5615eef46065dce81
Felicia Ebikon <38252721+Pearlicia@users.noreply.github.com>
2023-10-02 14:32:41 +0100

    #23 bucket should no longer use random provider (#24)

--- providers.tf
+++ providers.tf
@@ -7,17 +7,13 @@ terraform {
   #    name = "terra-house-1"
   #  }
   #}
-#   cloud {
-#    organization = "terraformer-bootcamp-2023"
-#    workspaces {
-#      name = "terra-house-1"
-#    }
-#   }
+  #cloud {
+  #  organization = "terraformer-bootcamp-2023"
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
     aws = {
       source = "hashicorp/aws"
       version = "5.16.2"
