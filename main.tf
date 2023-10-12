f16787866302f5601e71bbb6a7318b6363cd5577
Felicia Ebikon <feliciaebikon@gmail.com>
2023-10-12 21:14:59 +0000

    #48 fix issues with provider, setup two different towns.

--- main.tf
+++ main.tf
@@ -15,38 +15,56 @@ terraform {
   #    name = "terra-house-1"
   #  }
   #}
-  #cloud {
-  #  organization = "terraformer-bootcamp-2023""
-  #  workspaces {
-  #    name = "terra-house-1"
-  #  }
-  #}
+  cloud {
+   organization = "terraformer-bootcamp-2023"
+   workspaces {
+     name = "terra-house-1"
+   }
+  }
 }
 
 provider "terratowns" {
   endpoint = var.terratowns_endpoint
-  user_uuid = var.teacherseat_user_uuid 
+  user_uuid = var.teacherseat_user_uuid
   token = var.terratowns_access_token
 }
 
-
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
+module "home_akara_hosting" {
+  source = "./modules/terrahome_aws"
   user_uuid = var.teacherseat_user_uuid
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+  public_path = var.akara.public_path
+  content_version = var.akara.content_version
 }
 
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!"
+  name = "How to make akara!"
   description = <<DESCRIPTION
-    Just testing my missingo.
-    DESCRIPTION
-  domain_name = module.terrahouse_aws.cloudfront_url
-  town = "missingo"
-  content_version = 1
+Akara is a popular Nigerian street food made from black-eyed peas, also known as cowpeas. 
+It's a deep-fried bean cake that's crispy on the outside and soft on the inside. Here's a 
+simple recipe for making Nigerian Akara:
+DESCRIPTION
+  domain_name = module.home_akara_hosting.domain_name
+  town = "cooker-cove"
+  content_version = var.akara.content_version
 }
 
+module "home_pancake_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.pancake.public_path
+  content_version = var.pancake.content_version
+}
 
+resource "terratowns_home" "home_pancake" {
+  name = "Make home-made pancake"
+  description = <<DESCRIPTION
+A pancake is a flat, round, and typically thin batter-based dish, usually made 
+from a mixture of flour, eggs, milk or water, and a leavening agent. It's cooked on 
+a hot surface or pan and often served for breakfast or as a sweet or savory snack. 
+Pancakes are versatile and can be customized with various toppings like syrup, fruits, 
+chocolate chips, or whipped cream to suit individual preferences.
+DESCRIPTION
+  domain_name = module.home_pancake_hosting.domain_name
+  town = "cooker-cove"
+  content_version = var.pancake.content_version
+}
